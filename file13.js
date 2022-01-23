a8e41a3537740f033c71120f97888e4cbfc547eb
shubrajit acharjee <acharjeeshubrajit589@gmail.com>
2022-01-23 22:22:45 +0530

    initial commit

---
+++ file13.js
@@ -0,0 +1,9 @@
+// truthy falsy values
+
+// flasy values
+// false 
+// ""
+// null
+// undefined
+// 0
+
---
+++ file14.js
@@ -0,0 +1,38 @@
+// // if else condition
+// let age=100;
+// if (age>=18){
+//   console.log("user can watch porn");
+// }else{
+//   console.log("user can play mario");
+// }
+
+// let num=26;
+// if (num%2===0){
+//   console.log("input number is even")
+// }else{
+//   console.log("inputed number is odd")
+// }
+
+// flasy values
+// false 
+// ""
+// null
+// undefined
+// 0
+
+// truthy values
+// abcdef
+// 1, -1,-2
+
+
+let firstname="";
+// let firstname=null;
+// let firstname=false;
+// let firstname=0;
+// let firstname=undefined;
+
+if(firstname){
+console.log(firstname);
+}else{
+  console.log("firstname is empty");
+}
