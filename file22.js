4f2c75dbdb24f94771db963e9ddece04a660cdd1
shubrajit acharjee <acharjeeshubrajit589@gmail.com>
2022-01-26 23:54:03 +0530

    initial commit

--- file22.js
+++ file22.js
@@ -1,5 +1,17 @@
 // intro to for loop    
-for(let i=0; i<=9; i++){
-  console.log(i)
-}
+// for(var i=0; i<=9; i++){
+//   console.log(i)
+// }
+// // if we use let-->Uncaught ReferenceError: i is not defined
+//     at file22.js:6:30
+//     (anonymous) @ file22.js:6
+// If we use var --> there is no error when we  print the output on console out of the loop    
+// console.log("value of i is ",i);
 
+// we can use var like this
+let i=0;
+for (;i<=9; i++){
+  console.log(i);
+
+}
+console.log("value of i is ", i)
---
+++ file24.js
@@ -0,0 +1,12 @@
+// break keyward
+
+// continue keyward
+
+for (let i=1; i<=10; i++){
+  if (i===4){
+    break;
+  }
+  console.log(i);
+
+
+}
