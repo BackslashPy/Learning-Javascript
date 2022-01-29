639d534470fc76aba7dbd1bd2d2dfd82e6685d78
shubrajit acharjee <acharjeeshubrajit589@gmail.com>
2022-01-30 00:17:28 +0530

    array

--- file27.js
+++ file27.js
@@ -1,1 +1,24 @@
- 
+// array push pop  
+
+// array shift unshift
+let fruits=["apple","mango","grapes"];
+// console.log(fruits)
+//push
+fruits.push("orange");
+// // console.log(fruits)
+// console.log(fruits.pop());
+// console.log(fruits)
+let poppedfruit =fruits.pop();
+console.log(fruits)
+console.log("popped fruit is", poppedfruit)
+
+// unshift--> add eleet to starting
+fruits.unshift("heda")
+console.log(fruits)
+
+// shift-->reoe eleent from starting
+let removedfruit=fruits.shift();
+console.log(fruits);
+console.log("reoved fruit is " ,removedfruit);
+
+// push and pop method is faster than shift unshift
