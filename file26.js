a933d0f8bbce336a05379677af4a4556a7f5f131
shubrajit acharjee <acharjeeshubrajit589@gmail.com>
2022-01-29 23:54:10 +0530

    started array

--- file26.js
+++ file26.js
@@ -2,7 +2,15 @@
 // reference type
 
 // array is ordered collection of Elements
-let fruit=["apple", "ango", "grapes"];
+// odered collecto of items
+
+let fruit=["apple", "mango", "grapes"];
+let obj={}; //object literal
 console.log(fruit[0]);
 fruit[2]="hedabal"
 console.log(fruit)
+fruit[2]="banana";
+console.log(fruit)
+console.log(typeof fruit);
+console.log(Array.isArray(fruit)) // isarray to find out who is array or ot
+console.log(Array.isArray(obj))
