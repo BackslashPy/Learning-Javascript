37d901224cb95130b486001a9f7581c8c20ed696
shubrajit acharjee <acharjeeshubrajit589@gmail.com>
2022-02-06 07:40:18 +0530

    just chiling w/ js

---
+++ file28.js
@@ -0,0 +1,18 @@
+// primitive vs Reference data types
+// let num1=6;
+// let num2=num1;
+// console.log(num1)
+// console.log(num2)
+// num1++
+
+// console.log(num1)
+// console.log(num2)
+
+
+let array1=[1,2]
+let array2=array1
+console.log(array1)
+console.log(array2)
+array1.push(3)
+console.log(array1)
+console.log(array2)
