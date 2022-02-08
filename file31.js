6da91c383386c30428270952bdb98e94cb28a20a
shubrajit acharjee <acharjeeshubrajit589@gmail.com>
2022-02-08 14:06:04 +0530

    initial commit

--- file31.js
+++ file31.js
@@ -1,2 +1,12 @@
-// use const for creating Array
-const 
+// // use const for creating Array
+// const pi=3.14;
+// pi=12;
+// console.log(pi);
+
+// heap memory ["apple","mango"] 0x11
+
+const fruits=["apple","mango"]; //0x11
+fruits.push("banana")
+console.log(fruits)
+// you can push pop in a const
+// 0x11 is array addrss which is in heap memory
