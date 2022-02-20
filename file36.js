dbfdbfd07cc59cc218479b5b44fbe05b3c0762c5
shubrajit acharjee <acharjeeshubrajit589@gmail.com>
2022-02-20 14:24:11 +0530

    understanding objects

--- file36.js
+++ file36.js
@@ -6,6 +6,24 @@
 
 // how to create objects
 
-const person={name:"shubrajit", age:23};
+const person={
+  name:"shubrajit",
+  age:23,
+  hobbies:["coding","sleeping","listening musics"]
+
+
+
+};
 console.log(person)
+console.log(person.hobbies)
+
 // name is key shubrajit is value
+
+
+// how to access data from objects
+// console.log(person.name)
+// console.log(person.age)
+
+// how to add key value pair to objects
+person.gender="male"
+console.log(person)
