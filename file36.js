b3275fa8f109f17f3c17a842b7ab8faab9fb392f
shubrajit acharjee <acharjeeshubrajit589@gmail.com>
2022-02-20 13:28:17 +0530

    understanding objects

--- file36.js
+++ file36.js
@@ -1,2 +1,11 @@
 // objects
 // objects and arrays both are refference types
+// arrays are good nut not sufficient fro real world data
+// object store "key value" pairs
+// objects dont have index
+
+// how to create objects
+
+const person={name:"shubrajit", age:23};
+console.log(person)
+// name is key shubrajit is value
