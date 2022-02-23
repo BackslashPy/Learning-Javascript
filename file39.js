53e4cf5bc361518247515b50b868c7a7954fc7e0
shubrajit acharjee <acharjeeshubrajit589@gmail.com>
2022-02-23 14:08:51 +0530

    computed properties

---
+++ file39.js
@@ -0,0 +1,30 @@
+// computed properties
+
+const key1="objkey1";
+const key2="objkey2";
+
+const value1= "myvalue1";
+const value2= "myvalue2";
+
+// const obj={
+//   objkey1:"myvalue1",
+//   objkey2:"myvalue2",
+// }
+// another process
+
+// const obj={
+  // [key1]:value1,
+  // [key2]:value2,
+
+// }
+// console.log(obj);
+
+
+
+// another process
+const obj={};
+obj [key1]=value1
+obj [key2]=value2
+console.log(obj)
+
+
